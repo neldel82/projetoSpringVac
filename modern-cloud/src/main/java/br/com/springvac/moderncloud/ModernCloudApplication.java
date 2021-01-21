34f6cd6c64267df8c6ac93930255b57b91251615
ViniciusRibeiro <alves.vinicius@live.com>
2021-01-21 11:34:18 -0300

    Inicio do projeto!

---
+++ modern-cloud/src/main/java/br/com/springvac/moderncloud/ModernCloudApplication.java
@@ -0,0 +1,13 @@
+package br.com.springvac.moderncloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernCloudApplication.class, args);
+	}
+
+}
---
+++ modern-cloud/src/test/java/br/com/springvac/moderncloud/ModernCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springvac.moderncloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
